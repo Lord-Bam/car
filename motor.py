5496b58404e0121f5d99b80bd237a6fe5b49fdf8
BasMan <s.laget@scarlet.be>
2023-05-20 22:49:19 +0200

    finished with major refactor

--- motor.py
+++ motor.py
@@ -24,144 +24,111 @@ pwm4.duty(1023)
 
 #dir_en.off()
 
-def pulse_clock():
-    #time.sleep(0.05)   
+def pulse_clock():  
     dir_clk.value(1)
-    #time.sleep(0.05)
     dir_clk.value(0)
-    #time.sleep(0.05)
 
-def pulse_latch():
-    #time.sleep(0.05)    
+def pulse_latch():  
     dir_latch.value(1)
-    #time.sleep(0.05)
     dir_latch.value(0)
-    #time.sleep(0.05)
 
-def shift_write(value):
-
-    for x in range(0, 8):
-        temp = value & 128
-        print("temp=",temp, "/", end="")
-        if temp == 128:
-            dir_ser.on()           
+def shift_write(motorstates):
+    print(motorstates)
+    for motorstate in motorstates:
+        if motorstate == 1:
+            dir_ser.on()          
         else:
-            dir_ser.off()           
+            dir_ser.off()          
             
-        pulse_clock()        
-        value = value<<1     
+        pulse_clock()
         
     pulse_latch()
     print()
-
-
-def stop():
-    print("stop")    
-    shift_write(0b00000000)
     
-def m1_v():
-    print("m1_v")
-    # M4B/M3B/M4A/M2B/M1B/M1A/M2A/M3A
-    # QH / QG/ QF/ QE/QD /QC /QB /QA
-    # 0     0  0   0   0   1   0   0   >> hex  =  0x04
-    shift_write(0b00000100)
-    
-def m1_a():
-    print("m1_a")
-    # M4B/M3B/M4A/M2B/M1B/M1A/M2A/M3A
-    # QH / QG/ QF/ QE/QD /QC /QB /QA
-    # 0     0  0   0   1   0   0   0   >> hex  =  0x08
-    shift_write(0b00001000)
-def m2_v():
-    print("m2_v")
-    # M4B/M3B/M4A/M2B/M1B/M1A/M2A/M3A
-    # QH / QG/ QF/ QE/QD /QC /QB /QA
-    # 0     0  0   0   0   0   1   0   >> hex  =  0x02
-    shift_write(0b00000010)
-    
-def m2_a():
-    print("m2_a")
-    # M4B/M3B/M4A/M2B/M1B/M1A/M2A/M3A
-    # QH / QG/ QF/ QE/QD /QC /QB /QA
-    # 0     0  0   1   0   0   0   0   >> hex  =  0x10
-    shift_write(0b00010000)
-    
-def m3_v():
-    print("m3_v")
-    # M4B/M3B/M4A/M2B/M1B/M1A/M2A/M3A
-    # QH / QG/ QF/ QE/QD /QC /QB /QA
-    # 0     0  0   0   0   0   0   1   >> hex  =  0x01
-    shift_write(0b00100000)
-    
-def m3_a():
-    print("m3_a")
-    # M4B/M3B/M4A/M2B/M1B/M1A/M2A/M3A
-    # QH / QG/ QF/ QE/QD /QC /QB /QA
-    # 0     1  0   0   0   0   0   0   >> hex  =  0x40
-    shift_write(0b10000000)
-    
-def m4_v():
-    print("m4_v")
-    # M4B/M3B/M4A/M2B/M1B/M1A/M2A/M3A
-    # QH / QG/ QF/ QE/QD /QC /QB /QA
-    # 0     0  1   0   0   0   0   0   >> hex  =  0x20
-    shift_write(0b00000001)
-    
-def m4_a():
-    print("m4_a")
-    # M4B/M3B/M4A/M2B/M1B/M1A/M2A/M3A
-    # QH / QG/ QF/ QE/QD /QC /QB /QA
-    # 1     0  0   0   0   0   0   0   >> hex  =  0x80  
-    shift_write(0b01000000) 
-    
-    
-stop()
 msgstr = ""
 motorstates = [0,0,0,0,0,0,0,0]
+shift_write(motorstates)
 
 while True:
     msgstr = input()
+    old_motorstates = motorstates.copy()
     
-    
+    #motor1
     if msgstr == "1/v":
-        m1_v()
+        motorstates[4] = 0
+        motorstates[5] = 1
+
+    elif msgstr == "1/a":
+        motorstates[4] = 1
+        motorstates[5] = 0
         
     elif msgstr == "1/0":
-        stop()
+        motorstates[5] = 0
+        motorstates[4] = 0
         
-    elif msgstr == "1/a":
-        m1_a()
-        
-
+    #motor2
     elif msgstr == "2/v":
-        m2_v()
-        
-    elif msgstr == "2/0":
-        stop()
+        motorstates[3] = 0
+        motorstates[6] = 1
         
     elif msgstr == "2/a":
-        m2_a()
+        motorstates[3] = 1
+        motorstates[6] = 0
         
+    elif msgstr == "2/0":
+        motorstates[3] = 0
+        motorstates[6] = 0
+        
+    #motor3
     elif msgstr == "3/v":
-        m3_v()
-        
-    elif msgstr == "3/0":
-        stop()
+        motorstates[0] = 0
+        motorstates[2] = 1
         
     elif msgstr == "3/a":
-        m3_a()   
+        motorstates[0] = 1
+        motorstates[2] = 0
         
+    elif msgstr == "3/0":
+        motorstates[0] = 0
+        motorstates[2] = 0
+        
+    #motor4
     elif msgstr == "4/v":
-        m4_v()
-        
-    elif msgstr == "4/0":
-        stop()
+        motorstates[1] = 0
+        motorstates[7] = 1
         
     elif msgstr == "4/a":
-        m4_a()
+        motorstates[1] = 1
+        motorstates[7] = 0
+        
+    elif msgstr == "4/0":
+        motorstates[1] = 0
+        motorstates[7] = 0
+        
+    elif msgstr == "a/v":
+        motorstates = [0 for motor in motorstates]
+        motorstates[5] = 1
+        motorstates[6] = 1
+        motorstates[2] = 1
+        motorstates[7] = 1
+        
+        
+    elif msgstr == "a/a":
+        motorstates = [0 for motor in motorstates]
+        motorstates[4] = 1
+        motorstates[3] = 1
+        motorstates[0] = 1
+        motorstates[1] = 1
+    
+    elif msgstr == "a/0":
+        motorstates = [0 for motor in motorstates]
+
         
     else:
         print("verkeerd commando")
+        
+    if motorstates != old_motorstates:
+        shift_write(motorstates)
         
                 
 
